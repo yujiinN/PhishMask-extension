b8ef0234c6f86d3a1ae3648ed90f0fef47f28a98
Ryan <150203547+NotAProgrammer187@users.noreply.github.com>
2023-11-08 09:00:31 +0800

    Add files via upload

---
+++ Phishing Detector System Extension/button.js
@@ -0,0 +1,20 @@
+// Wait for the document to fully load
+document.addEventListener("DOMContentLoaded", function () {
+    // Function to handle the "Open Logs" button click
+    document.querySelector(".logs-button").addEventListener("click", function () {
+        // Replace this with your code to open logs or perform the desired action
+        alert("Open Logs button clicked");
+    });
+
+    // Function to handle the "Report Phishing" button click
+    document.getElementById("reportButton").addEventListener("click", function () {
+        // Replace this with your code to report phishing or perform the desired action
+        alert("Report Phishing button clicked");
+    });
+
+    // Function to handle the "Feedback" button click
+    document.getElementById("feedbackButton").addEventListener("click", function () {
+        // Replace this with your code to collect feedback or perform the desired action
+        alert("Feedback button clicked");
+    });
+});
