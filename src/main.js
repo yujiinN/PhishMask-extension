4c8f95da1f3500cf72aa4ce9adcbc0bf0dd3a6cb
Ryan <ryancamado@gmail.com>
2023-11-11 13:19:36 +0800

    idk
    fixing stuffs

--- src/main.js
+++ src/main.js
@@ -13,29 +13,3 @@ document.addEventListener("DOMContentLoaded", function () {
     });
   });
 
-  document.addEventListener("DOMContentLoaded", function () {
-    let blockedCount = 0;
-  
-    function updateBlockedCount() {
-      const blockedCountElement = document.getElementById("blockedCount");
-      blockedCountElement.textContent = `(${blockedCount} ${blockedCount === 1 ? 'website' : 'websites'})`;
-    }
-  
-    // Function to simulate detecting a phishing website
-    function detectPhishingWebsite() {
-      // Replace this logic with your actual phishing website detection logic
-      const isPhishing = Math.random() < 0.5; // Simulating a random detection result
-  
-      if (isPhishing) {
-        blockedCount++;
-        updateBlockedCount();
-        alert("Phishing website detected! Blocked count increased.");
-      } else {
-        alert("No phishing website detected.");
-      }
-    }
-  
-    // Example: Call the detectPhishingWebsite function when needed
-    // In a real scenario, you would call this function when your detection logic determines a phishing website.
-    detectPhishingWebsite();
-  });
