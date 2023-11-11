561f3d20ee9af750f0513ecb03d15c1d2fbc24fa
Blaineeey <124621386+Blaineeey@users.noreply.github.com>
2023-11-11 16:14:50 +0800

    fixed UI
    tan awa daw

--- src/feedback.js
+++ src/feedback.js
@@ -1,26 +1,44 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var submitButton = document.querySelector(".btSubmit");
-    var nameInput = document.getElementById("name");
-    var emailInput = document.getElementById("email");
-    var feedbackTextarea = document.getElementById("feedback");
-    var feedbackMessageContainer = document.getElementById("feedbackMessageContainer");
-  
-    submitButton.addEventListener("click", function () {
-      var name = nameInput.value;
-      var email = emailInput.value;
-      var feedback = feedbackTextarea.value;
-  
-      if (name.trim() !== "" && email.trim() !== "" && feedback.trim() !== "") {
-        window.location.href = "message.html";
+  var submitButton = document.querySelector(".btSubmit");
+  var nameInput = document.getElementById("name");
+  var emailInput = document.getElementById("email");
+  var feedbackTextarea = document.getElementById("feedback");
+  var feedbackMessageContainer = document.getElementById("feedbackMessageContainer");
 
-        // Optionally, you can clear the form fields
-        nameInput.value = "";
-        emailInput.value = "";
-        feedbackTextarea.value = "";
-      } else {
-        var errorMessage = "Please fill in all fields.";
-        feedbackMessageContainer.textContent = errorMessage;
-      }
-    });
+  submitButton.addEventListener("click", function () {
+    var name = nameInput.value;
+    var email = emailInput.value;
+    var feedback = feedbackTextarea.value;
+
+    if (name.trim() !== "" && email.trim() !== "" && feedback.trim() !== "") {
+      alert("Feedback submitted successfully!");
+
+      setTimeout(function () {
+        feedbackMessageContainer.textContent = "";
+        window.location.href = "home.html";
+      }, 1000);
+
+      nameInput.value = "";
+      emailInput.value = "";
+      feedbackTextarea.value = "";
+    } else {
+      var errorMessage = "Please fill in all fields.";
+      feedbackMessageContainer.textContent = errorMessage;
+    }
   });
-  
+});
+
+// Function to go back to home.html
+function goBack() {
+  window.location.href = "home.html";
+}
+
+// Function to open logs (adjust the URL as needed)
+function openLogs() {
+  window.location.href = "logs.html";
+}
+
+// Function to report phishing (adjust the URL as needed)
+function reportPhishing() {
+  window.location.href = "report.html";
+}
--- src/report.js
+++ src/report.js
@@ -1,25 +1,39 @@
 document.addEventListener("DOMContentLoaded", function () {
-    var submitButton = document.querySelector(".btSubmit");
-    var reportTextarea = document.getElementById("report");
-    var reportMessageContainer = document.getElementById("reportMessageContainer");
-  
-    submitButton.addEventListener("click", function () {
-      var phishingURL = reportTextarea.value;
-  
-      if (phishingURL.trim() !== "") {
-        alert("Phishing link reported successfully!");
-  
-        setTimeout(function () {
-          reportMessageContainer.textContent = "";
-          window.location.href = "home.html";
-        }, 1000);
-  
-        // Optionally, you can clear the form field
-        reportTextarea.value = "";
-      } else {
-        var errorMessage = "Please enter a phishing URL.";
-        reportMessageContainer.textContent = errorMessage;
-      }
-    });
+  var submitButton = document.querySelector(".btSubmit");
+  var reportTextarea = document.getElementById("report");
+  var reportMessageContainer = document.getElementById("reportMessageContainer");
+
+  submitButton.addEventListener("click", function () {
+    var phishingURL = reportTextarea.value;
+
+    if (phishingURL.trim() !== "") {
+      alert("Phishing link reported successfully!");
+
+      setTimeout(function () {
+        reportMessageContainer.textContent = "";
+        goBack(); // Call the goBack function to navigate back to home.html
+      }, 1000);
+
+      reportTextarea.value = "";
+    } else {
+      var errorMessage = "Please enter a phishing URL.";
+      reportMessageContainer.textContent = errorMessage;
+    }
   });
-  
+});
+
+// Function to go back to home.html
+function goBack() {
+  window.location.href = "home.html";
+}
+
+// Function to open logs
+function openLogs() {
+  // Replace this with the actual functionality to open logs
+  alert("Opening Logs");
+}
+
+// Function to open feedback
+function openFeedback() {
+  window.location.href = "feedback.html"; // Navigate to feedback.html
+}
